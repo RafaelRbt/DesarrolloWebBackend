49a0ba6b9af92d6335c0799069447101e16328a9
rafael romero bello <a01747730@itesm.mx>
2022-04-07 20:37:42 -0500

    prueba 1

--- app.py
+++ app.py
@@ -9,3 +9,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if (request.method == "GET"):
+        return render_template("Login.html", data="email")
+    else:
+        email = None
+        email = request.form["email"]
+        password = request.form["password"]
+        return render_template("index.html", data=email)
+
+
